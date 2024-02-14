9d4d6c29f2bd3637f47c7bc3e6dabe875536064a
motion <motionsutra51@gmail.com>
2024-02-14 17:27:27 +0530

    Second Revision

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
+const loader = document.querySelector(".loader");
+const main = document.getElementById("main");
 const video = document.getElementById("video1");
-const text = SplitType.create('.greeting_text');
 const soundIcon = document.querySelector(".fa-solid");
 const page3 = document.getElementById("page3");
 const page3Circle = document.querySelector(".illutrations_circle");
-console.log(soundIcon)
 
+//Loading Script
+window.addEventListener("load",()=>{
+    loader.style.visibility = "hidden";
+    main.style.visibility = "visible";
+})
+
+$(document).ready(function(){
+    $("#menu").click(function(){
+        $(".menu_page").slideDown();
+        $(this).hide();
+    });
+    $(".close_menu").click(function(){
+        $(".menu_page").slideUp();
+        $("#menu").show();
+    })
+})
+
+// Splite Type
+const text = SplitType.create('.greeting_text');
 text.chars;
+const softwareText = SplitType.create("#software_text");
+softwareText.chars;
 
 gsap.to("#craft_story",{
     xPercent: -100,
@@ -41,10 +62,73 @@ function sound(){
 
 page3.addEventListener("mousemove", (dets)=>{
     // console.log(dets)
-    console.log(dets.clientX)
+    // console.log(dets.clientX)
     gsap.to(".illutrations_circle",{
         x: dets.clientX - 40,
         y: dets.clientY -40 
     })
 })
+
+const page5 = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#page5",
+        start: "top 80%",
+        end: "top 15%",
+        markers: true,
+        scrub: 2
+    }
+})
+.from("#software_text .char",{
+    y: 30,
+    opacity: 0,
+    stagger: {
+        amount:1
+    }
+
+})
+.from(".page5_main_content1",{
+    x: 50,
+    opacity: 0
+})
+.from(".page5_main_content1 h2",{
+    y: -35,
+    opacity: 0
+})
+.from(".video_editing_logo",{
+    y: 35,
+    opacity: 0,
+    stagger: {
+        amount: 0.5
+    }
+})
+.from(".page5_main_content2",{
+    x: -50,
+    opacity: 0
+})
+.from(".page5_main_content2 h2",{
+    y: -35,
+    opacity: 0
+})
+.from(".graphic_designing_logo",{
+    y: 35,
+    opacity: 0,
+    stagger: {
+        amount: 0.5
+    }
+})
+// gsap.from("#software_text .char",{
+//     y: 30,
+//     opacity: 0,
+//     stagger: {
+//         amount:1
+//     },
+//     scrollTrigger: {
+//         trigger: "#page5",
+//         start: "top 80%",
+//         end: "top 70%",
+//         markers: true,
+//         scrub: 2
+//     }
+   
+// })
 
